ca34516a97580ccd3d55d97c7c148bab533d4731
Romanda <imromanda@gmail.com>
2025-06-09 20:45:58 +0200

    Change HTML format

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
-Hola, soy Romanda
-
-Este es mi primer sitio web y está en construcción. 
-
+<body>
+	<H1>¡Hola!</H1>
+	<p>
+		<br>Soy Romanda <br> Este es mi primer sitio web y está en construcción. 
+<br>
 Pronto más novedades.
-
+<br>
 <?php
 date_default_timezone_set('europe/madrid');
 echo date('d-m-Y H:i:s');
 ?>
+					</p>
+				</body>
