3cddb2617f9cbe15c3b67fc946a96eb72e08a6cb
Romanda <imromanda@gmail.com>
2025-06-09 20:17:59 +0200

    Page initial description

---
+++ index.php
@@ -0,0 +1,10 @@
+Hola, soy Romanda
+
+Este es mi primer sitio web y está en construcción. 
+
+Pronto más novedades.
+
+<?php
+date_default_timezone_set('europe/madrid');
+echo date('d-m-Y H:i:s');
+?>
